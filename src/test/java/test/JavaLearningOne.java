0e63e3c050b6ccd0d1485836cf2b6e7ad9dafa68
gratra <gauravratra@hotmail.com>
2022-08-03 23:06:50 +0100

    adding check to git

--- src/test/java/test/JavaLearningOne.java
+++ src/test/java/test/JavaLearningOne.java
@@ -3,5 +3,6 @@ package test;
 public class JavaLearningOne {
     public static void main(String[] args) {
         System.out.println("Learning Java for automation");
+        System.out.println("Checking to update the git repo via terminal");
     }
 }
